0a6eee83a06ce2b098ba2cd6cbab033824ed6970
mk <midaslmg94@gmail.com>
2020-06-08 17:17:25 +0900

    좋아요 음악 가져오기

--- server/src/main/java/wing/api/user/client/LikedMusicClient.java
+++ server/src/main/java/wing/api/user/client/LikedMusicClient.java
@@ -1,4 +1,27 @@
 package wing.api.user.client;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import wing.api.user.domain.LikedMusic;
+
+import java.util.List;
+
+@Component
 public class LikedMusicClient {
+    public List<LikedMusic> getLikedMusicList(String userId){
+        RestTemplate restTemplate = new RestTemplate();
+        String serviceUri = String.format("http://localhost:8003/api/user/liked/%s",userId);
+        ResponseEntity<List<LikedMusic>> restExchange = restTemplate.exchange(
+                serviceUri,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<LikedMusic>>() {
+                },
+                userId
+        );
+        return restExchange.getBody();
+    }
 }
--- server/src/main/java/wing/api/user/domain/LikedMusic.java
+++ server/src/main/java/wing/api/user/domain/LikedMusic.java
@@ -1,4 +1,14 @@
 package wing.api.user.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LikedMusic {
+//    private String id;
+//    private String userId;
+    private String musicName;
+    private String artistName;
+
 }
